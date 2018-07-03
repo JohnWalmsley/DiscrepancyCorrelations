6f4108a5da9315f73cc7c3787b1a4895d22d08cd
Unknown <johnwalmsley@gmail.com>
2018-07-03 16:51:18 +0100

    Updated prediction of discrepancy
    PredictDiscrepancyUsingLinearModel accepts as inputs:
    exp_ref: a cell number
    training_protocol: protocol used to train the discrepancy model, usually sine_wave prediction_protocol: protocol to be predicted, usually ap
    method: one of 'lasso', 'stepwisefit','regress', 'least-squares', 'stepwiselm'
    only_core: binary variable. if 1, ignores the activation / deactivation steps at the beginning and end of the protocol.
    conf:  can also be edited inside the function, often easier in practice. Contains variables used by BuildDataMatrix.m
    
    
    LinearModelOfDiscrepancy has been updated to perform cross-validation using high resolution time series data. The standard matlab implementation selects folds at random. In this high time resolution data, the result would be that the data sets would be expected to differ only in terms of experimental noise. The code now splits the data into ten equally sized consecutive chunks. To do this, an edit to the matlab source is required as follows (use sudo, the file is read-only):
    
    usr/local/MATLAB/R2017a/toolbox/stats/stats/+internal/+stats/cvpartitionImpl.m
    
    and change on line 113
       properties(GetAccess = protected, SetAccess = protected)
    to
       properties(GetAccess = public, SetAccess = public)
    
    then you can access and set the indices used for cross-validation using:
    cvp = cvpartition( discrepancy, 'K', numPartitions );    
    cvp.Impl.indices = partitionIdxs;
    cvp.Impl.indices is a vector of length equal to your data, whose entries correspond to the group the point is assigned to.
    
    The code has also been altered to allow manipulation of the lasso plots by returning the handle to the lambda vs MSE plots.
    
    There is also an option to remove linearly dependent indices when using stepwiselm
    
    The config option 'thin' can be used to reduce the resolution of the data used.

--- Code/LinearModelOfDiscrepancyWithInput.m
+++ Code/LinearModelOfDiscrepancyWithInput.m
@@ -1,12 +1,21 @@
-function [ discrepancy, data_matrix, model ] = ...
+function [ discrepancy, data_matrix, model, fig ] = ...
                         LinearModelOfDiscrepancyWithInput( method, conf, discrepancy, data_matrix )
 %LINEARMODELOFDISCREPANCY Summary of this function goes here
 %   Detailed explanation goes here
+
+fig = [];
 
 % Discrepancy = model*data
 if (~any( strcmp( conf, 'const' ) ) && strcmp( method, 'regress' ) )
     disp( 'Warning: Method regess requires a constant vector, adding to data matrix' )
     data_matrix = [ ones( size( time ) ) data_matrix ];
+end
+
+if any( strcmp( conf, 'thin' ) )
+    num_rows = size( data_matrix, 1 );
+    idx = 1 : 10 : num_rows;
+    data_matrix = data_matrix(idx,:);
+    discrepancy = discrepancy( idx );
 end
 
 data_matrix_corr = data_matrix;
@@ -21,7 +30,7 @@ elseif strcmp( method, 'stepwisefit' )
     else
         disp( 'Warning: Method stepwise requires a constant vector, adding to data matrix')
         data_matrix_corr_sw = data_matrix_corr;
-        data_matrix = [ ones( size( time) ) data_matrix ];
+        data_matrix = [ ones( size( data_matrix, 1), 1 ) data_matrix ];
     end
     [ b,~,~,inmodel,stats,~,~ ] = stepwisefit( data_matrix_corr_sw, discrepancy, 'penter', 0.0005, 'premove', 0.01 );
     intercept = stats.intercept;
@@ -34,21 +43,54 @@ elseif strcmp( method, 'lasso' )
         data_matrix_corr_lasso = data_matrix_corr( :, 2 : end );
     else
         disp( 'Warning: Method lasso requires a constant vector, adding to data matrix')
-        data_matrix = [ ones( size( time) ) data_matrix ];
+        data_matrix = [ ones( size( data_matrix, 1), 1 ) data_matrix ];
         data_matrix_corr_lasso = data_matrix_corr;
     end
-    [ models, fitinfo ] = lasso( data_matrix_corr_lasso, discrepancy, 'CV', 10, 'DFmax', 10 );
-    lassoPlot(models,fitinfo,'PlotType','CV');
+    % Build a fixed partition of the data
+
+    % NOTE: THIS WILL ONLY WORK IF /usr/local/MATLAB/R2017a/toolbox/stats/stats/+internal/+stats/cvpartitionImpl.m 
+    % has been edited so that indices are no longer 'protected', but instead are 'public'!
+    numPartitions = 10;
+    partition = cvpartition( discrepancy, 'K', numPartitions );
+    numDataPoints = length( discrepancy );
+    partitionIdxs = ones( size(discrepancy ) );
+    numPerGroup = ceil( length( discrepancy ) / numPartitions );
+    numOver = numPerGroup * numPartitions - numDataPoints;
+    
+    % Sort into groups of size as close as possible to
+    % #discrepancy/numPartitions
+    for k = 1 : numPartitions - numOver
+        partitionIdxs( (k-1)* numPerGroup + 1 : k*numPerGroup ) = k * ones( numPerGroup, 1 );  
+    end
+    for k = numPartitions - numOver + 1 : numPartitions
+        init_idx = (numPartitions - numOver)*numPerGroup;
+        npg = numPerGroup-1;
+        k1 = k-(numPartitions - numOver);
+        partitionIdxs(  init_idx + (k1-1)*npg + 1 : init_idx+k1*npg ) = k * ones( numPerGroup-1, 1 );  
+    end
+    partition.Impl.indices = partitionIdxs;
+       
+    size(partitionIdxs)
+    size( discrepancy)
+    
+	[ models, fitinfo ] = lasso( data_matrix_corr_lasso, discrepancy, 'CV', partition );
+    %[ models, fitinfo ] = lasso( data_matrix_corr_lasso, discrepancy, 'CV', 'resubstitution' );
+
+    [ ~, fig ] = lassoPlot(models,fitinfo,'PlotType','CV');
+
     intercepts = fitinfo.Intercept;
     intercept = intercepts( fitinfo.Index1SE );    
     model = [ intercept; models( :, fitinfo.Index1SE ) ];
+    
 elseif strcmp( method, 'stepwiselm' )
     % note - strcmp does not take the constant vector as input:
     if any( strcmp( conf, 'const' ) )
         data_matrix_corr_swlm = data_matrix_corr( :, 2 : end );
     end
-    model = stepwiselm( data_matrix_corr_swlm, discrepancy, 'quadratic', 'Criterion', 'bic', 'Verbose', 2 );
-    model
+    [~, lin_indep_idx, ~] = getLinearIndependent( data_matrix_corr_swlm, 1 );
+    exclude_idx = 1 : size( data_matrix_corr_swlm, 2 );
+    exclude_idx(lin_indep_idx) = []; % just leaves the dependent idxs.
+    model = stepwiselm( data_matrix_corr_swlm, discrepancy, 'constant', 'Criterion', 'aic', 'Upper', 'linear', 'Verbose', 1);% 'exclude', exclude_idx );
 else
     disp('Method not valid, proceeding with Least Squares' )
     model = data_matrix_corr \ discrepancy;
---
+++ Code/PredictDiscrepancyUsingLinearModel.m
@@ -0,0 +1,493 @@
+function PredictDiscrepancyUsingLinearModel( exp_ref, training_protocol, prediction_protocol, method, only_core, conf )
+
+
+%% Defining the discrepancy in the open probability:
+% G * (O+d)*(V-V_E) = data
+% Therefore data - Isim = G*d*(V-V_E)
+% and so d = ( data-Isim ) / (G*(V-V_E)) outside V_E + eps
+
+addpath( genpath( '../../SharedFunctions' ) )
+addpath ../../../MathworksFileExchange/getLinearDependent/
+
+%% Configuration
+
+if nargin < 5
+    only_core = 0;
+end
+
+if nargin < 6
+    conf = { 'const', 'variables', 'I', 'rates', 'fluxes', 'sensitivity', 'dIdp', 'voltage', 'dVdt', 'time', 'normalise', 'thin' };
+    %conf = { 'const', 'variables', 'I', 'rates', 'dIdp', 'voltage', 'dVdt', 'time', 'normalise', 'thin' };
+    %conf = { 'const', 'variables', 'I', 'rates', 'fluxes', 'dIdp', 'voltage', 'dVdt', 'time', 'normalise' };
+    %conf = { 'const', 'variables' };
+end
+
+% eps determines voltage proximity to reversal when calculating discrepancy
+% in open probability
+eps = 5;
+
+%% Training Data
+
+% Calculate variables and discrepancies
+
+[ variables_tr, I_tr, rates_tr, fluxes_tr, sensitivity_tr, dIdp_tr, ~, dVdt_tr, time_tr, G ]  ...
+                            = CalculateVariables( training_protocol, exp_ref );
+[ discrepancy_tr, exp_data_tr ] = CalculateDiscrepancy( exp_ref, training_protocol, I_tr );
+voltage_tr=importdata(['../Protocols/' training_protocol '_protocol.mat']);
+
+% Set up constants 
+F = 96485;
+R = 8314;
+K_i = 130;
+k_o = 4;
+T = GetTemperature( exp_ref ) + 273.5;
+erev = ((R*T)/F)*log(k_o/K_i);
+
+%--------------------------------------------------------------------------
+% Discrepancy
+%--------------------------------------------------------------------------
+
+% Remove the pacing spikes from the simulated and experimental data
+
+idx_nospike = GetNoSpikeIdx( training_protocol, length( time_tr ) );
+if only_core == 1
+    idx_core = GetCoreOfProtocolIdx( training_protocol );
+else
+    idx_core = 1 : length( voltage_tr );
+end
+idx_d = intersect( idx_nospike, idx_core );
+
+% variables for calculating discrepancy model
+voltage_tr_d = voltage_tr( idx_d );
+time_tr_d = time_tr( idx_d );
+variables_tr_d = variables_tr( idx_d, : );
+I_tr_d = I_tr(idx_d);
+rates_tr_d = rates_tr( idx_d, :) ;
+fluxes_tr_d = fluxes_tr( idx_d, : );
+exp_data_tr_d = exp_data_tr( idx_d );
+sensitivity_tr_d = sensitivity_tr( idx_d, :);
+dIdp_tr_d = dIdp_tr(idx_d,:);
+dVdt_tr_d = dVdt_tr(idx_d, :);
+discrepancy_tr_d = discrepancy_tr( idx_d );
+
+% calculate rates fluxes sensitivity
+[data_matrix_tr_d, factors_tr_d ] = BuildDataMatrix( conf, variables_tr_d, I_tr_d, rates_tr_d, fluxes_tr_d, ...
+                                                       sensitivity_tr_d, dIdp_tr_d, voltage_tr_d, dVdt_tr_d, time_tr_d );                                                
+                                                   
+[ ~, data_matrix_tr_d_used, model_d, lassofig_d ] = ...
+                        LinearModelOfDiscrepancyWithInput( method, conf, discrepancy_tr_d, data_matrix_tr_d );
+
+if ~isa( model_d, 'LinearModel' )
+    included_idx = find( abs( model_d ) > 0 );
+    names = GetNames( conf );
+    included_names = names( included_idx );
+end
+% predicted discrepancy
+if isa( model_d, 'LinearModel' )
+    if any( strcmp( conf, 'const' ) )
+        disc_modelled = predict( model_d, data_matrix_tr_d( :, 2 : end ) );
+    else
+        disc_modelled = predict( model_d, data_matrix_tr_d ); 
+    end
+    %model.Coefficients
+else
+    disc_modelled = data_matrix_tr_d*model_d;
+end
+
+%--------------------------------------------------------------------------
+% Discrepancy in Open
+%--------------------------------------------------------------------------
+                               
+% Remove the indices near the reversal potential for Odisc
+idx_erev = find( abs( voltage_tr - erev ) > eps );
+idx_od = intersect( idx_d, idx_erev );
+
+voltage_tr_od = voltage_tr( idx_od );
+time_tr_od = time_tr( idx_od );
+variables_tr_od = variables_tr( idx_od, : );
+I_tr_od = I_tr(idx_od);
+rates_tr_od = rates_tr( idx_od, :) ;
+fluxes_tr_od = fluxes_tr( idx_od, : );
+exp_data_tr_od = exp_data_tr( idx_od );
+sensitivity_tr_od = sensitivity_tr( idx_od, :);
+dIdp_tr_od = dIdp_tr(idx_od,:);
+dVdt_tr_od = dVdt_tr(idx_od, :);
+
+discrepancy_tr_od = discrepancy_tr( idx_od ) ./ ( G * ( voltage_tr( idx_od ) - erev ) );
+
+% Construct the discrepancy model
+
+% calculate rates fluxes sensitivity
+[data_matrix_tr_od, factors_tr_od ] = BuildDataMatrix( conf, variables_tr_od, I_tr_od, rates_tr_od, fluxes_tr_od, ...
+                                                        sensitivity_tr_od, dIdp_tr_od, voltage_tr_od, dVdt_tr_od, time_tr_od );
+[ ~, data_matrix_tr_od_used, model_od, lassofig_od ] = ...
+                        LinearModelOfDiscrepancyWithInput( method, conf, discrepancy_tr_od, data_matrix_tr_od );
+
+                    
+if ~isa( model_od, 'LinearModel' )
+    included_idx = find( abs( model_od ) > 0 );
+    names = GetNames( conf );
+    included_names = names( included_idx );
+end
+% predicted discrepancy in O
+if isa( model_od, 'LinearModel' )
+    if any( strcmp( conf, 'const' ) )
+       odisc_modelled = predict( model_od, data_matrix_tr_od( :, 2 : end ) );
+    else
+       odisc_modelled = predict( model_od, data_matrix_tr_od ); 
+    end
+    %model.Coefficients
+else
+    odisc_modelled = data_matrix_tr_od*model_od;
+    
+end
+                 
+%% Prediction Data
+
+[ variables_pr, I_pr, rates_pr, fluxes_pr, sensitivity_pr, dIdp_pr, voltage_pr, dVdt_pr, time_pr, ~ ]  ...
+                                    = CalculateVariables( prediction_protocol, exp_ref );
+[ discrepancy_pr, exp_data_pr ] = CalculateDiscrepancy( exp_ref, prediction_protocol, I_pr );
+voltage_pr=importdata(['../Protocols/' prediction_protocol '_protocol.mat']);
+
+%--------------------------------------------------------------------------
+% Discrepancy
+%--------------------------------------------------------------------------
+
+% Remove the pacing spikes from the simulated and experimental data
+idx_nospike = GetNoSpikeIdx( prediction_protocol, length( time_pr ) );
+if only_core == 1
+    idx_core = GetCoreOfProtocolIdx( prediction_protocol );
+else
+    idx_core = 1 : length( voltage_pr );
+end
+idx_d = intersect( idx_nospike, idx_core );
+
+% variables for evaluating discrepancy model
+voltage_pr_d = voltage_pr( idx_d );
+time_pr_d = time_pr( idx_d );
+variables_pr_d = variables_pr( idx_d, : );
+I_pr_d = I_pr(idx_d);
+rates_pr_d = rates_pr( idx_d, :) ;
+fluxes_pr_d = fluxes_pr( idx_d, : );
+exp_data_pr_d = exp_data_pr( idx_d );
+sensitivity_pr_d = sensitivity_pr( idx_d, :);
+dIdp_pr_d = dIdp_pr(idx_d,:);
+dVdt_pr_d = dVdt_pr(idx_d, :);
+discrepancy_pr_d = discrepancy_pr( idx_d );
+
+% Build the data matrix used for prediction
+[data_matrix_pr_d, factors_pr_d ] = BuildDataMatrix( conf, variables_pr_d, I_pr_d, rates_pr_d, fluxes_pr_d, ...
+                                                       sensitivity_pr_d, dIdp_pr_d, voltage_pr_d, dVdt_pr_d, time_pr_d );
+
+if isa( model_d, 'LinearModel' )
+    if any( strcmp( conf, 'const' ) )
+        disc_predicted = predict( model_d, data_matrix_pr_d( :, 2 : end ) );
+    else
+        disc_predicted = predict( model_d, data_matrix_pr_d ); 
+    end
+else
+    disc_predicted = data_matrix_pr_d*model_d;
+    included_idxs_d = abs(model_d) > 0;
+    num_included_d = sum( included_idxs_d );
+    names_included_d = names( included_idxs_d );
+end
+                                                   
+%--------------------------------------------------------------------------
+% Discrepancy in Open
+%--------------------------------------------------------------------------
+                                                   
+% Remove the indices near the reversal potential for Odisc
+idx_erev = find( abs( voltage_pr - erev ) > eps );
+idx_od = intersect( idx_d, idx_erev );
+
+voltage_pr_od = voltage_pr( idx_od );
+time_pr_od = time_pr( idx_od );
+variables_pr_od = variables_pr( idx_od, : );
+I_pr_od = I_pr(idx_od);
+rates_pr_od = rates_pr( idx_od, :) ;
+fluxes_pr_od = fluxes_pr( idx_od, : );
+exp_data_pr_od = exp_data_pr( idx_od );
+sensitivity_pr_od = sensitivity_pr( idx_od, :);
+dIdp_pr_od = dIdp_pr(idx_od,:);
+dVdt_pr_od = dVdt_pr(idx_od, :);
+
+discrepancy_pr_od = discrepancy_pr( idx_od ) ./ ( G * ( voltage_pr( idx_od ) - erev ) );
+
+% Construct the discrepancy model
+
+% Build the data matrix used for prediction
+[data_matrix_pr_od, factors_pr_od ] = BuildDataMatrix( conf, variables_pr_od, I_pr_od, rates_pr_od, fluxes_pr_od, ...
+                                                        sensitivity_pr_od, dIdp_pr_od, voltage_pr_od, dVdt_pr_od, time_pr_od );
+
+if isa( model_od, 'LinearModel' )
+    if any( strcmp( conf, 'const' ) )
+        odisc_predicted = predict( model_od, data_matrix_pr_od( :, 2 : end ) );
+    else
+        odisc_predicted = predict( model_od, data_matrix_pr_od ); 
+    end
+else
+    odisc_predicted = data_matrix_pr_od*model_od;
+    included_idxs_od = abs(model_od) > 0;
+    num_included_od = sum( included_idxs_od );
+    names_included_od = names( included_idxs_od );
+end
+
+%% Save data and plot results
+
+current_time = clock;
+current_time = fix( current_time );
+
+dirname = [ 'FittedModels/FitModel_' method '_tr_' training_protocol '_pr_' prediction_protocol '_' date ...
+                '_' num2str( current_time( 4 ) ) '-' num2str( current_time( 5 ) ) '-' num2str( current_time( 6 ) ) ];
+mkdir( dirname )
+cd( dirname )
+
+% save the config files and setup
+save( 'config.mat', 'conf', 'only_core', 'method', 'training_protocol', 'prediction_protocol' );
+
+% save the model of discrepancy
+save( 'model_d.mat', 'model_d', 'data_matrix_tr_d', 'data_matrix_pr_d' )
+% save the model of open-discrepancy
+save( 'model_od.mat', 'model_od', 'data_matrix_tr_od', 'data_matrix_pr_od' )
+
+fig_currents = figure( 'Units', 'Normalized', 'OuterPosition', [ 0 0 1 1 ] );
+
+% detemine axes
+time_min = min( [ min(time_pr_d), min(time_tr_d), min(time_pr_od), min(time_pr_d) ] );
+time_max = max( [ max(time_pr_d), max(time_tr_d), max(time_pr_od), max(time_pr_d) ] );
+
+d_min = min( [ min(I_tr_d), min( exp_data_tr_d ), min( I_tr_d + disc_modelled ) ...
+               min(I_pr_d), min( exp_data_pr_d ), min( I_pr_d + disc_predicted )] );
+d_max = max( [ max(I_tr_d), max( exp_data_tr_d ), max( I_tr_d + disc_modelled ) ...
+               max(I_pr_d), max( exp_data_pr_d ), max( I_pr_d + disc_predicted )] );
+ylim_d = 0.5*( [ d_min d_max ] );
+
+od_min = min( [ min(I_tr_od), min( exp_data_tr_od ), min( I_tr_od + G*odisc_modelled.*( voltage_tr_od - erev ) ) ...
+               min(I_pr_od), min( exp_data_pr_od ), min( I_pr_od + G*odisc_predicted.*( voltage_pr_od - erev ) )] );
+od_max = max( [ max(I_tr_od), max( exp_data_tr_od ), max( I_tr_od + G*odisc_modelled.*( voltage_tr_od - erev ) ) ...
+               max(I_pr_od), max( exp_data_pr_od ), max( I_pr_od + G*odisc_predicted.*( voltage_pr_od - erev ) )] );
+ylim_od = 0.5*( [ od_min od_max ] );
+
+% make plot of current with discrepancy
+subplot( 2,2,1 )
+plot( time_tr_d, exp_data_tr_d, 'r' )
+hold on
+plot( time_tr_d, I_tr_d, 'b' )
+plot( time_tr_d, I_tr_d + disc_modelled, 'k' ) 
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ training_protocol, ' training d' ], 'Interpreter', 'None' )
+ylim(ylim_d);
+
+disc_base_tr_d = sqrt( sum( ( exp_data_tr_d - I_tr_d ).^2 ) );
+disc_model_tr_d = sqrt( sum( ( exp_data_tr_d - ( I_tr_d + disc_modelled ) ).^2 ) );
+
+legend( { 'Iexp', [ 'Isim ' num2str( disc_base_tr_d ) ], [ 'Isim+d '  num2str( disc_model_tr_d ) ] })
+
+subplot( 2,2,2 )
+plot( time_tr_od, exp_data_tr_od, 'r' )
+hold on
+plot( time_tr_od, I_tr_od, 'b' )
+plot( time_tr_od, I_tr_od + G*odisc_modelled.*( voltage_tr_od - erev ), 'k' ) 
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ training_protocol, ' training od' ], 'Interpreter', 'None' )
+ylim(ylim_od);
+
+disc_base_tr_od = sqrt( sum( ( exp_data_tr_od - I_tr_od ).^2 ) );
+disc_model_tr_od = sqrt( sum( ( exp_data_tr_od - ( I_tr_od + G*odisc_modelled.*( voltage_tr_od - erev ) ) ).^2 ) );
+
+legend( { 'Iexp', [ 'Isim ' num2str( disc_base_tr_od ) ], [ 'Isim+G.Od.(V-E) '  num2str( disc_model_tr_od ) ] })
+
+subplot( 2,2,3 )
+plot( time_pr_d, exp_data_pr_d, 'r' )
+hold on
+plot( time_pr_d, I_pr_d, 'b' )
+plot( time_pr_d, I_pr_d + disc_predicted, 'k' )
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ prediction_protocol, ' prediction d' ], 'Interpreter', 'None' )
+ylim(ylim_d);
+
+disc_base_pr_d = sqrt( sum( ( exp_data_pr_d - I_pr_d ).^2 ) );
+disc_model_pr_d = sqrt( sum( ( exp_data_pr_d - ( I_pr_d + disc_predicted ) ).^2 ) );
+
+legend( { 'Iexp', [ 'Isim ' num2str( disc_base_pr_d ) ], [ 'Isim+d '  num2str( disc_model_pr_d ) ] })
+
+subplot( 2,2,4 )
+plot( time_pr_od, exp_data_pr_od, 'r' )
+hold on
+plot( time_pr_od, I_pr_od, 'b' )
+plot( time_pr_od, I_pr_od + G*odisc_predicted.*( voltage_pr_od - erev ), 'k' ) 
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ prediction_protocol, ' prediction od' ], 'Interpreter', 'None' )
+ylim(ylim_od);
+
+disc_base_pr_od = sqrt( sum( ( exp_data_pr_od - I_pr_od ).^2 ) );
+disc_model_pr_od = sqrt( sum( ( exp_data_pr_od - ( I_pr_od + G*odisc_predicted.*( voltage_pr_od - erev ) ) ).^2 ) );
+
+legend( { 'Iexp', [ 'Isim ' num2str( disc_base_pr_od ) ], [ 'Isim+G.Od.(V-E) '  num2str( disc_model_pr_od ) ] })
+
+% Beautify plot
+set( fig_currents, 'Color', 'w' );
+set(findall( fig_currents, 'type', 'axes'), 'Box', 'off' );
+set(findall( fig_currents, 'type', 'axes'), 'FontName','Arial','FontSize',12,'FontWeight','Bold',  'LineWidth', 2);
+set(findall( fig_currents, 'type', 'axes'), 'xLim', [ time_min time_max]);
+
+export_fig( fig_currents, [ 'CurrentWithDiscrepancy.tif' ], '-tif' )
+
+%--------------------------------------------------------------------------
+% make plot of discrepancy
+
+d_min = min( [ min(discrepancy_tr_d), min( disc_modelled ) ...
+               min( discrepancy_pr_d), min( disc_predicted )] );
+d_max = max( [ max(discrepancy_tr_d), max( disc_modelled ) ...
+               max(discrepancy_pr_d), max( disc_modelled )] );
+ylim_d = 0.5*( [ d_min d_max ] );
+
+od_min = min( [ min(discrepancy_tr_od), min( odisc_modelled ) ...
+               min(discrepancy_pr_od), min( odisc_predicted )] );
+od_max = max( [ max(discrepancy_tr_od), max( odisc_modelled ) ...
+               max(discrepancy_pr_od), max(odisc_predicted )] );
+ylim_od = ( [ od_min od_max ] );
+
+
+subplot( 2,2,1 )
+plot( time_tr_d, discrepancy_tr_d, 'b' )
+hold on
+plot( time_tr_d, disc_modelled, 'k' ) 
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ training_protocol, ' training d' ], 'Interpreter', 'None' )
+ylim(ylim_d);
+
+disc_base_tr_d = sqrt( sum( discrepancy_tr_d.^2 ) );
+disc_model_tr_d = sqrt( sum( (discrepancy_tr_d-disc_modelled).^2 ) );
+
+legend( { [ 'Iexp-Isim ' num2str( disc_base_tr_d ) ], [ 'Iexp-(Isim+d) '  num2str( disc_model_tr_d ) ] })
+
+subplot( 2,2,2 )
+plot( time_tr_od, discrepancy_tr_od, 'b' )
+hold on
+plot( time_tr_od, odisc_modelled, 'k' ) 
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ training_protocol, ' training od' ], 'Interpreter', 'None' )
+ylim(ylim_od);
+
+disc_base_tr_od = sqrt( sum( discrepancy_tr_od.^2 ) );
+disc_model_tr_od = sqrt( sum( (discrepancy_tr_od-odisc_modelled).^2 ) );
+
+legend( { [ 'Odexp ' num2str( disc_base_tr_od ) ], [ 'Odexp-Od '  num2str( disc_model_tr_od ) ] })
+
+subplot( 2,2,3 )
+plot( time_pr_d, discrepancy_pr_d, 'b' )
+hold on
+plot( time_pr_d, disc_predicted, 'k' )
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ prediction_protocol, ' prediction d' ], 'Interpreter', 'None' )
+ylim(ylim_d);
+
+disc_base_pr_d = sqrt( sum( discrepancy_pr_d.^2 ) );
+disc_model_pr_d = sqrt( sum( (discrepancy_pr_d-disc_predicted).^2 ) );
+
+legend( {[ 'Iexp-Isim ' num2str( disc_base_pr_d ) ], [ 'Iexp-(Isim+d) '  num2str( disc_model_pr_d ) ] })
+
+subplot( 2,2,4 )
+plot( time_pr_od, discrepancy_pr_od, 'b' )
+hold on
+plot( time_pr_od, odisc_predicted, 'k' ) 
+hold off
+xlabel( 'Time (ms)' )
+ylabel( 'Current (nA)' )
+title( [ prediction_protocol, ' prediction od' ], 'Interpreter', 'None' )
+ylim(ylim_od);
+
+disc_base_pr_od = sqrt( sum( ( discrepancy_pr_od).^2 ) );
+disc_model_pr_od = sqrt( sum( ( discrepancy_pr_od - odisc_predicted ).^2 ) );
+
+legend( {[ 'Odexp ' num2str( disc_base_pr_od ) ], [ 'Odexp-Od '  num2str( disc_model_pr_od ) ] })
+
+% Beautify plot
+set( fig_currents, 'Color', 'w' );
+set(findall( fig_currents, 'type', 'axes'), 'Box', 'off' );
+set(findall( fig_currents, 'type', 'axes'), 'FontName','Arial','FontSize',12,'FontWeight','Bold',  'LineWidth', 2);
+set(findall( fig_currents, 'type', 'axes'), 'xLim', [ time_min time_max]);
+
+export_fig( fig_currents, [ 'Discrepancy.tif' ], '-tif' )
+
+%--------------------------------------------------------------------------
+if ~isempty( lassofig_d )
+    figure(lassofig_d )
+    title( 'Cross-validated MSE of Lasso fit: d')
+    set( gcf, 'Color', 'w' );
+    set(findall( gcf, 'type', 'axes'), 'Box', 'off' );
+    set(findall( gcf, 'type', 'axes'), 'FontName','Arial','FontSize',12,'FontWeight','Bold',  'LineWidth', 2);
+    
+    export_fig( lassofig_d, 'LassoMSE_d.tif', '-tif' )
+end
+
+if ~isempty( lassofig_od )
+    figure(lassofig_od )
+    title( 'Cross-validated MSE of Lasso fit: od')
+    set( gcf, 'Color', 'w' );
+    set(findall( gcf, 'type', 'axes'), 'Box', 'off' );
+    set(findall( gcf, 'type', 'axes'), 'FontName','Arial','FontSize',12,'FontWeight','Bold',  'LineWidth', 2);
+    
+    export_fig( lassofig_od, 'LassoMSE_od.tif', '-tif' )
+end
+
+%--------------------------------------------------------------------------
+
+% Bar chart of coefficients
+
+fig_coeffs = figure( 'Units', 'Normalized', 'OuterPosition', [ 0 0 1 1 ] );
+subplot( 1,2,1)
+i = find( model_d == 0 );
+model_d_bar = model_d;
+model_d_bar( i ) = [];
+
+bar( model_d_bar )
+xtick=get(gca,'xtick'); 
+xMax=max(xtick); 
+xMin=min(xtick); 
+newXTick=linspace(xMin,xMax,num_included_d); 
+set(gca,'xtick', newXTick );
+xticklabels( names_included_d )
+xtickangle( 90 )
+title( 'Coefficients d' )
+
+subplot( 1,2,2)
+i = find( model_od == 0 );
+model_od_bar = model_od;
+model_od_bar( i ) = [];
+
+bar( model_od_bar )
+xtick=get(gca,'xtick'); 
+xMax=max(xtick); 
+xMin=min(xtick); 
+newXTick=linspace(xMin,xMax,num_included_od); 
+set(gca,'xtick', newXTick );
+xticklabels( names_included_od )
+xtickangle( 90 )
+title( 'Coefficients od' )
+
+set( gcf, 'Color', 'w' );
+set(findall( gcf, 'type', 'axes'), 'Box', 'off' );
+set(findall( gcf, 'type', 'axes'), 'FontName','Arial','FontSize',12,'FontWeight','Bold',  'LineWidth', 2);
+
+export_fig( fig_coeffs, 'coeffs.tif', '-tif' )
+
+% Back to code
+cd ../..
+
+end
