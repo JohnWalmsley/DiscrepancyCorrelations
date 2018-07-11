1db9f9656ed721895efc529022036e1ffeda31cb
Unknown <johnwalmsley@gmail.com>
2018-07-11 11:52:48 +0100

    BuildDataMatrix removes linear dependent terms
    Uses getLinearIndependent from Mathworks file exchange. Removes linearly dependent terms from each class of input (variables, rates etc). Also returns the names of each variable in the final data matrix.

--- Code/BuildDataMatrix.m
+++ Code/BuildDataMatrix.m
@@ -1,10 +1,17 @@
-function [ mx, factors ] = BuildDataMatrix( conf, variables, I, rates, fluxes, ...
+function [ mx, factors, names ] = BuildDataMatrix( conf, variables, I, rates, fluxes, ...
                                             sensitivity, dIdp, voltage, dVdt, time, input_factors )
 %BUILDDATAMATRIX Summary of this function goes here
 %   Detailed explanation goes here
 
 mx = [];
 normalise = any(strcmp( conf, 'normalise' ) );
+
+% Note - others only have one entry
+[~, var_ind_idx, ~]= getLinearIndependent(variables, 1);
+[~, rate_ind_idx, ~]= getLinearIndependent(rates, 1);
+[~, flux_ind_idx, ~]= getLinearIndependent(fluxes, 1);
+[~, sens_ind_idx, ~]= getLinearIndependent(sensitivity, 1);
+[~, dIdp_ind_idx, ~]= getLinearIndependent(dIdp, 1);
 
 if normalise
     if nargin == 11 % useful when making predictions from a model built on different data
@@ -71,35 +78,57 @@ end
 factors = [ min_fac_var min_fac_I, min_fac_rate, min_fac_flux, min_fac_sens, min_fac_dIdp, min_fac_V, min_fac_dVdt, min_fac_t;... 
             max_fac_var max_fac_I, max_fac_rate, max_fac_flux, max_fac_sens, max_fac_dIdp, max_fac_V, max_fac_dVdt, max_fac_t];
 
+names = {};        
+        
 if any(strcmp( conf, 'const' ) )
     mx = [ mx ones( length( I ), 1 ) ];
+    name = GetNames( 'const' );
+    names = [ names, name ];
 end
-if any(strcmp( conf, 'variables' ) )
-    mx = [ mx ( variables - min_fac_var ) ./ (max_fac_var - min_fac_var ) ];
+if any(strcmp( conf, 'variables' ) )    
+    mx = [ mx ( variables( :, var_ind_idx ) - min_fac_var( var_ind_idx ) ) ./ (max_fac_var( var_ind_idx ) - min_fac_var( var_ind_idx ) ) ];
+    name = GetNames( 'variables' );
+    names = [ names, name( var_ind_idx ) ];
 end
 if any(strcmp( conf, 'I' ) )
     mx = [ mx ( I - min_fac_I ) ./ ( max_fac_I - min_fac_I ) ];
+    name = GetNames( 'I' );
+    names = [ names, name ];
 end
 if any(strcmp( conf, 'rates' ) )
-    mx = [ mx ( rates - min_fac_rate ) ./ ( max_fac_rate - min_fac_rate ) ];
+    mx = [ mx ( rates( :, rate_ind_idx ) - min_fac_rate( rate_ind_idx ) ) ./ ( max_fac_rate( rate_ind_idx ) - min_fac_rate( rate_ind_idx ) ) ];
+    name = GetNames( 'rates' );
+    names = [ names, name( rate_ind_idx ) ];
 end
 if any(strcmp( conf, 'fluxes' ) )
-    mx = [ mx ( fluxes - min_fac_flux ) ./ (max_fac_flux - min_fac_flux ) ];
+    mx = [ mx ( fluxes( :, flux_ind_idx ) - min_fac_flux( flux_ind_idx ) ) ./ (max_fac_flux( flux_ind_idx ) - min_fac_flux( flux_ind_idx ) ) ];
+    name = GetNames( 'fluxes' );
+    names = [ names, name( flux_ind_idx ) ];
 end
 if any(strcmp( conf, 'sensitivity' ) )
-    mx = [ mx ( sensitivity - min_fac_sens ) ./ ( max_fac_sens - min_fac_sens ) ];
+    mx = [ mx ( sensitivity( :, sens_ind_idx ) - min_fac_sens( sens_ind_idx ) ) ./ ( max_fac_sens( sens_ind_idx ) - min_fac_sens( sens_ind_idx ) ) ];
+    name = GetNames( 'sensitivity' );
+    names = [ names, name( sens_ind_idx ) ];
 end
 if any(strcmp( conf, 'dIdp' ) )
-    mx = [ mx ( dIdp - min_fac_dIdp ) ./ ( max_fac_dIdp - min_fac_dIdp ) ];
+    mx = [ mx ( dIdp( :, dIdp_ind_idx ) - min_fac_dIdp( dIdp_ind_idx ) ) ./ ( max_fac_dIdp( dIdp_ind_idx ) - min_fac_dIdp( dIdp_ind_idx ) ) ];
+    name = GetNames( 'dIdp' );
+    names = [ names, name( dIdp_ind_idx ) ];
 end
 if any(strcmp( conf, 'voltage' ) )
     mx = [ mx ( voltage - min_fac_V ) ./ ( max_fac_V - min_fac_V ) ];
+    name = GetNames( 'voltage' );
+    names = [ names, name ];
 end
 if any(strcmp( conf, 'dVdt' ) )
     mx = [ mx ( dVdt - min_fac_dVdt ) ./ ( max_fac_dVdt - min_fac_dVdt ) ];
+    name = GetNames( 'dVdt' );
+    names = [ names, name ];
 end
 if any(strcmp( conf, 'time' ) )
     mx = [ mx ( time - min_fac_t ) ./ ( max_fac_t - min_fac_t ) ];
+    name = GetNames( 'time' );
+    names = [ names, name ];
 end
     
 end
